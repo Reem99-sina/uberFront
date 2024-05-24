363057a5b5a1b0fd1d7fe159da512de43ed79504
Reem99-sina <reemsina2@gmail.com>
2024-05-24 23:36:34 +0300

    godd

--- screens/ProfileScreens.jsx
+++ screens/ProfileScreens.jsx
@@ -1,14 +1,18 @@
 import { Image, Pressable, StyleSheet, Text, View } from "react-native";
 import NavItem from "../components/NavItem";
-import { getItem } from "../utils/AsyncStorage";
+import { getItem,clear } from "../utils/AsyncStorage";
 import { useState, useEffect } from "react";
-import Icon from "react-native-vector-icons/FontAwesome";
 
-function ProfileScreen() {
+import ButtonCustom from "../components/common/ButtonCustom";
+import Icon from "react-native-vector-icons/FontAwesome";
+function ProfileScreen({navigation}) {
   let [user, setuser] = useState();
 
   async function getUser() {
     setuser(await getItem("user"));
+  }
+  async function removeUser() {
+    await clear()
   }
   useEffect(() => {
     getUser();
@@ -33,6 +37,10 @@ function ProfileScreen() {
         <Text style={{ fontSize: 18,margin:20 }}>
           {user?.phoneNumber}
         </Text>
+        <ButtonCustom styleButton={styles.styleButton} onPress={()=>{removeUser();navigation.navigate("Home")}}>
+        <Text style={styles.styleText}>Log out</Text>
+        <Icon name="arrow-right" size={20} color={"#fff"} />
+      </ButtonCustom>
     </View>
   );
 }
@@ -40,10 +48,21 @@ export default ProfileScreen;
 const styles = StyleSheet.create({
   container: {
     padding: 30,
+  }, styleButton: {
+    backgroundColor: "#000",
+    width: "90%",
+    justifyContent: "space-around",
+    borderRadius: 16,
+    padding: 20,
+    flexDirection: "row"
   },
   avatar: {
     width: 50,
     height: 50,
     borderRadius: 25,
+  },  styleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#fff",
   },
 });
